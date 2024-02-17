41a668b6250a9d4ed4f2998e9ab0d08ee055c3fa
Ondicho albert <ondicho53@gmail.com>
2024-02-17 20:54:56 +0300

    services icons

--- src/data/ServicesData.js
+++ src/data/ServicesData.js
@@ -1,25 +1,30 @@
+import web from '../assets/images/web-development.png'
+import system from '../assets/images/system-architecture.png'
+import database from '../assets/images/database.png'
+import devops from '../assets/images/devops.png'
+
 const ServicesData = [
     {
       title: 'Web Development',
-      icon: '',
+      icon: web,
       description: 'Design, Develop, and Deploy beautiful, responsive, and user-friendly websites.',
       stack: ['HTML', 'CSS', 'JavaScript', 'React']
     },
     {
       title: 'System Design & Architecture',
-      icon: '',
+      icon: system,
       description: 'Design and implement robust and scalable system architectures.',
       stack: ['Microservices', 'Docker', 'Kubernetes']
     },
     {
       title: 'Database Design & Management',
-      icon: '',
+      icon: database,
       description: 'Design and manage databases for efficient data storage and retrieval.',
       stack: ['SQL', 'NoSQL', 'PostgreSQL', 'MongoDB']
     },
     {
       title: 'DevOps',
-      icon: '',
+      icon: devops,
       description: 'Automate and streamline the software development lifecycle.',
       stack: ['CI/CD', 'Jenkins', 'Git', 'Docker']
     }
